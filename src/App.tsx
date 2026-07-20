16db35a1252c4d48c93dab0f27159c46c0bb3eb1
Maria Eduarda <mariaeduardadantaslp@gmail.com>
2026-07-19 21:21:54 -0300

    feat: implementação das páginas AboutPomodoro, NotFound e Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Button } from "./components/Button";
+
+import { Home } from "./pages/Home";
 
 export function App() {
-  return (
-    <>
-      <Heading>
-        Olá mundo!
-        <Button icone="trash"></Button>
-      </Heading>
-    </>
-  );
+  return <Home />;
 }
